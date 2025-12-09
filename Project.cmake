991c8cc80c2c45431d11fc2d088c15be3c3b1e52
Winteradio <tot4766@hanyang.ac.kr>
2025-12-09 21:30:47 +0900

    [Title] Edit the library function that handle the library type
    [Desc]
    [Type/Branch]
    [Auther/Data]

--- Project.cmake
+++ Project.cmake
@@ -2,9 +2,30 @@
 message(STATUS "# Set the basic options for the project")
 
 function(project_options)
+	cmake_parse_arguments(
+		ARG # Options / Single Value / Multi Value
+		""
+		"TARGET_TYPE;"
+		""
+		${ARGN})
+
+	if (NOT DEFINED ARG_TARGET_TYPE)
+		set(ARG_TARGET_TYPE "PRIVATE")
+	endif()
+
+	if (${ARG_TARGET_TYPE} STREQUAL "PUBLIC")
+		set(TARGET_TYPE PUBLIC)
+	elseif (${ARG_TARGET_TYPE} STREQUAL "PRIVATE")
+		set(TARGET_TYPE PRIVATE)
+	elseif (${ARG_TARGET_TYPE} STREQUAL "INTERFACE")
+		set(TARGET_TYPE INTERFACE)
+	else()
+		message(FATAL_ERROR "# Fatal Error : The target type(${ARG_TARGET_TYPE}) is invalid")
+	endif()
+
 	message(STATUS "- Compiler : ${CMAKE_CXX_COMPILER_ID}")
 
-	target_compile_features(${PROJECT_NAME} PRIVATE cxx_std_17)
+	target_compile_features(${PROJECT_NAME} ${TARGET_TYPE} cxx_std_17)
 
 	if (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
 		add_compile_options(/Zi /MP)
